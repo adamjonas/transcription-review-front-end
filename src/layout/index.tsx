14ccf885fa9056ba751114bbfe6e81892141a5eb
Adam Jonas <jonas@chaincode.com>
2023-11-14 21:09:53 -0500

    add umami

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -11,25 +11,31 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const { noRestriction } = useNoContainerLimit();
   const router = useRouter();
   const isHomePage = router.asPath === "/";
+  const isReviewSite = router.basePath === "https://review.btctranscripts.com";
   return (
-    <div className={styles.app_container}>
-      <Head>
-        <title>Bitcoin Transcription Reviews</title>
-        <meta name="description" content="Bitcoin Transcription Review" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./btc-transcript-circle-128.png" />
-      </Head>
-      {!isHomePage && <Navbar />}
-      <GlobalContainer
-        flexGrow={1}
-        py={!isHomePage ? 4 : 0}
-        mt={!isHomePage ? 12 : 0}
-        {...(noRestriction ? { maxW: "none", p: 0 } : {})}
-      >
-        {children}
-      </GlobalContainer>
-      {!isHomePage && <Footer />}
-    </div>
+    <>
+      <div className={styles.app_container}>
+        <Head>
+          <title>Bitcoin Transcription Reviews</title>
+          <meta name="description" content="Bitcoin Transcription Review" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="./btc-transcript-circle-128.png" />
+        </Head>
+        {!isHomePage && <Navbar />}
+        <GlobalContainer
+          flexGrow={1}
+          py={!isHomePage ? 4 : 0}
+          mt={!isHomePage ? 12 : 0}
+          {...(noRestriction ? { maxW: "none", p: 0 } : {})}
+        >
+          {children}
+        </GlobalContainer>
+        {!isHomePage && <Footer />}
+      </div>
+      {isReviewSite && (
+        <script async src="https://visits.bitcoindevs.xyz/script.js" data-website-id="d9b96a7b-a2db-4ef1-9360-69d3b288859d"></script>
+      )}
+    </>
   );
 };
 
